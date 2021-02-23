f5241e2b72e50752ab915ca5f59de80962ecb1b7
JUNGHOJONG <hojong1351@naver.com>
2021-02-23 22:28:07 +0900

    3.4 등록/수정/조회 API 만들기

---
+++ src/main/java/com/davinci/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package com.davinci.springboot.web.dto;
+
+import com.davinci.springboot.domain.posts.Posts;
+
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getTitle();
+        this.author = entity.getAuthor();
+    }
+}
